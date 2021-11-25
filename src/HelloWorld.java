0b0f92fa9fa3519946884dc58b76e7922c54f9a2
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-11-24 22:34:35 -0600

    first 5 hackerrank java problems

---
+++ src/HelloWorld.java
@@ -0,0 +1,127 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+
+//TODO
+//  1.      Output Format: You must print two lines of output:
+//
+//  Print Hello, World. on the first line.
+//  Print Hello, Java. on the second line.
+//          Solution:
+//        System.out.println("Hello, World.");
+//        System.out.println("Hello, Java.");
+
+
+//TODO
+//  2.   In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+//
+//Input Format
+//
+//There are 3 lines of input, and each line contains a single integer.
+//
+//        Solution:
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        int b = scan.nextInt();
+//        int c = scan.nextInt();
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//    }
+
+
+//TODO
+//  3.   Given an integer, , perform the following conditional actions:
+//
+//        If  is odd, print Weird
+//        If  is even and in the inclusive range of  to , print Not Weird
+//        If  is even and in the inclusive range of  to , print Weird
+//        If  is even and greater than , print Not Weird
+//        Complete the stub code provided in your editor to print whether or not  is weird.
+//
+//                Input Format
+//
+//        A single line containing a positive integer, .
+//
+//        Constraints
+//
+//        Output Format
+//
+//        Print Weird if the number is weird; otherwise, print Not Weird.
+//
+//      Solution:
+//        int n = scanner.nextInt();
+//        String answer = "";
+//
+//        if(n % 2 == 1) {
+//            answer = "Weird";
+//        }else{
+//            if(n>=6 && n<=20) {
+//                answer = "Weird";
+//            } else {
+//                answer = "Not Weird";
+//            }
+//        }
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//        System.out.println(answer);
+//        scanner.close();
+
+//TODO 4. Input Format
+//
+//        There are three lines of input:
+//
+//        The first line contains an integer.
+//        The second line contains a double.
+//                The third line contains a String.
+//                Output Format
+//
+//        There are three lines of output:
+//
+//        On the first line, print String: followed by the unaltered String read from stdin.
+//        On the second line, print Double: followed by the unaltered double read from stdin.
+//        On the third line, print Int: followed by the unaltered integer read from stdin.
+//        To make the problem easier, a portion of the code is already provided in the editor.
+//
+//        Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens; because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() will be reading the remainder of the integer line (which is empty).
+//
+//  Solution:
+//                Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d = scan.nextDouble();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//        scan.close();
+//
+//        // Write your code here.
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
+
+//TODO 5. Input Format
+//        Every line of input will contain a String followed by an integer.
+//        Each String will have a maximum of  alphabetic characters, and each integer will be in the inclusive range from  to .
+//
+//        Output Format
+//
+//        In each line of output there should be two columns:
+//        The first column contains the String and is left justified using exactly  characters.
+//                The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+//
+// Solution:
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++)
+//        {
+//            String s1=sc.next();
+//            int x=sc.nextInt();
+//            System.out.printf("%-15s%03d\n", s1, x);
+//        }
+//        System.out.println("================================");
+
+
+    }
+}
