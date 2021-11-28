dc7096a63512405f31c1f86c1df86c2e6bfc47fc
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-11-28 17:59:33 -0600

    java EOF hackerrank problem

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -199,7 +199,24 @@ public class HelloWorld {
 //            }
 //        }
 
-
+//TODO Java End Of File
+// 9.
+//        Input Format
+//
+//        Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+//
+//                Output Format
+//
+//        For each line, print the line number, followed by a single space, and then the line content received as input.
+//
+//
+//        Solution
+//                Scanner sc = new Scanner (System.in);
+//        int i = 1;
+//        while (sc.hasNext()) {
+//            System.out.println(i++ + " " + sc.nextLine());
+//        }
+//        sc.close();
 
     }
 }
