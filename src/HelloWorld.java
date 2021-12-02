af016a42b902057a5062ccf465e021b1e1c33920
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-12-01 18:08:33 -0600

    Java date time problem

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -261,5 +261,42 @@ public class HelloWorld {
 //
 //     }//end of class
 
+
+//TODO Java Date and Time
+// 11.
+//                You are given a date. You just need to write the method, , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
+//
+//
+//
+// Solution
+//
+//  import java.time.LocalDate;
+//  import java.time.DayOfWeek;
+//
+//      class Result {
+//
+//    /*
+//     * Complete the 'findDay' function below.
+//     *
+//     * The function is expected to return a STRING.
+//     * The function accepts following parameters:
+//     *  1. INTEGER month
+//     *  2. INTEGER day
+//     *  3. INTEGER year
+//     */
+//
+//    public static String findDay(int month, int day, int year) {
+//        LocalDate localDate = LocalDate.of(year, month, day);
+//    java.time.DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+//    System.out.println(dayOfWeek);
+//    return dayOfWeek.toString();
+//    }
+//
+//}
+
+
+
+
+
     }
 }
