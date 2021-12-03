db01a00a150a3e5b3ffe9c23c98ecbe08c940489
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-12-03 11:04:28 -0600

    java currency hackerrank problem

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -310,7 +310,27 @@ public class HelloWorld {
 //
 // }
 
-
-
+//TODO Java Currency Formatter
+// 13.
+//
+// Solution
+//
+//      Scanner scanner = new Scanner(System.in);
+//        double payment = scanner.nextDouble();
+//        scanner.close();
+//
+//        // Write your code here.
+//        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+//        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+//        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+//        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+//
+//        System.out.println("US: " + us);
+//        System.out.println("India: " + india);
+//        System.out.println("China: " + china);
+//        System.out.println("France: " + france);
+//
+//
+//
     }
 }
