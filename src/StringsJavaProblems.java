265539d785a6abd86907d305eb35b45e589c95cd
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-06 18:09:27 -0600

    started java strings problems

---
+++ src/StringsJavaProblems.java
@@ -0,0 +1,19 @@
+public class StringsJavaProblems {
+    public static void main(String[] args) {
+
+//TODO Java Strings Intro
+//  1.      For the first line, sum the lengths of A and B.
+//For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
+//For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.
+//
+//  Scanner sc=new Scanner(System.in);
+//        String A=sc.next();
+//        String B=sc.next();
+//        /* Enter your code here. Print output to STDOUT. */System.out.println(A.length()+B.length());
+//System.out.println(A.compareTo(B)>0?"Yes":"No");
+//System.out.println(A.substring(0, 1).toUpperCase()+A.substring(1, A.length())+" "+B.substring(0, 1).toUpperCase()+B.substring(1, B.length()));
+
+
+
+    }
+}
