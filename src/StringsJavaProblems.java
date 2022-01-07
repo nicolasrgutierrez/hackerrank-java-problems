0ae39da2f6bd71d4a7f0db7d1192a3944dc09249
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-07 16:55:56 -0600

    java substring problem

--- src/StringsJavaProblems.java
+++ src/StringsJavaProblems.java
@@ -9,9 +9,25 @@ public class StringsJavaProblems {
 //  Scanner sc=new Scanner(System.in);
 //        String A=sc.next();
 //        String B=sc.next();
-//        /* Enter your code here. Print output to STDOUT. */System.out.println(A.length()+B.length());
+//        /* Enter your code here. Print output to STDOUT. */
+//        System.out.println(A.length()+B.length());
 //System.out.println(A.compareTo(B)>0?"Yes":"No");
 //System.out.println(A.substring(0, 1).toUpperCase()+A.substring(1, A.length())+" "+B.substring(0, 1).toUpperCase()+B.substring(1, B.length()));
+
+
+//TODO Java Substring
+// 2.  The first line contains a single string denoting .
+//The second line contains two space-separated integers denoting the respective values of START and END.
+//
+//  Scanner in = new Scanner(System.in);
+//        String S = in.next();
+//        int start = in.nextInt();
+//        int end = in.nextInt();
+//                System.out.println(S.substring(start,end));
+
+
+// TODO
+// 3.
 
 
 
