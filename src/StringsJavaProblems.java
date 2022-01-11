ab6d99b4b4be264d18964f1ffa29a8f613fbebaf
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-11 14:05:50 -0600

    java anagram problem

--- src/StringsJavaProblems.java
+++ src/StringsJavaProblems.java
@@ -47,6 +47,7 @@ public class StringsJavaProblems {
 //    }
 //
 
+
 //TODO Java String Reverse
 // 4. Given a string A, print Yes if it is a palindrome, print No otherwise.
 //
@@ -58,6 +59,38 @@ public class StringsJavaProblems {
 //
 
 
-// TODO
+// TODO Java Anagrams
+//  5. Complete the isAnagram function in the editor.
+//
+//isAnagram has the following parameters:
+//
+//string a: the first string
+//string b: the second string
+// Returns Boolean: if A and B are case-insensitive anagrams, return true. Otherwise, return false.
+//
+//   static boolean isAnagram(String a, String b) {
+//        // Complete the function
+//         a=a.toUpperCase();
+//        b=b.toUpperCase();
+//        boolean ret = false;
+//        StringBuilder c= new StringBuilder(b);
+//
+//        if(a.length()==b.length()){
+//            for(int i=0; i<a.length();i++){
+//                for(int j=0; j<c.length();j++){
+//                    if(a.charAt(i)==c.charAt(j)){
+//                            c.deleteCharAt(j);
+//                        if(i==a.length()-1 && c.length()==0){
+//                            ret=true;
+//                            break;
+//                        }
+//                        break;
+//                    }
+//                }
+//            }
+//        }return ret;
+//
+//    }
+
     }
 }
