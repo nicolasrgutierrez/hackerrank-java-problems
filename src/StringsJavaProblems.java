6925ee02009a8194bfbe64ea182d93961548d0ec
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-14 14:06:40 -0600

    pattern syntax checker solution

--- src/StringsJavaProblems.java
+++ src/StringsJavaProblems.java
@@ -114,6 +114,29 @@ public class StringsJavaProblems {
 //
 
 
-//TODO
+//TODO Pattern Syntax Checker
+//Input Format
+//
+//The first line of input contains an integer N, denoting the number of test cases. The next N lines contain a string of any printable characters representing the pattern of a regex.
+//
+//Output Format
+//
+//For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
+//
+//Scanner in = new Scanner(System.in);
+//		int testCases = Integer.parseInt(in.nextLine());
+//		while(testCases>0){
+//			String pattern = in.nextLine();
+//            try{
+//              Pattern p = Pattern.compile(pattern);
+//              System.out.println("Valid");
+//          }catch(Throwable t){
+//              System.out.println("Invalid");
+//          }
+//          testCases--;
+//          	//Write your code
+//		}
+
+
     }
 }
