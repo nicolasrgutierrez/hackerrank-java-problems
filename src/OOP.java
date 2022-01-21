1f89a8aa9c65d37f0008219e2857611717cedb33
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-21 12:25:46 -0600

    started OOP problems on hackerrank to refresh

---
+++ src/OOP.java
@@ -0,0 +1,13 @@
+public class OOP {
+
+//TODO
+// Java Abstract Class
+// Create another class, MyBook, that extends the abstract class Book within the exercise
+//
+// class MyBook extends Book {
+//    @Override
+//    void setTitle(String s){
+//        this.title = s;
+//    }
+//}
+}
