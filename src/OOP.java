c1cc8d8e23ea6c13b188e08c1c97988a36b17fd5
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-23 12:39:27 -0600

    OOP java interface solution

--- src/OOP.java
+++ src/OOP.java
@@ -10,4 +10,27 @@ public class OOP {
 //        this.title = s;
 //    }
 //}
+
+
+//TODO
+// Java Interface
+// You are given an interface AdvancedArithmetic which contains a method signature int divisor_sum(int n). You need to write a class called MyCalculator which implements the interface.
+//Read the partially completed code in the editor and complete it. You just need to write the MyCalculator class only. Your class shouldn't be public.
+//class MyCalculator implements AdvancedArithmetic {
+//    public int divisor_sum(int n) {
+//        if (n <= 1) { return n; }
+//
+//        int res = n + 1;
+//        for (int i = 2; i < n; i++) {
+//            if(n % i == 0) {
+//                res += i;
+//            }
+//        }
+//        return res;
+//    }
+//}
+//
+
+
+// TODO
 }
