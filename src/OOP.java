17bd331f4b7fdd7a502c7386d6d976243a015eec
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-01-25 21:04:47 -0600

    java super override solution

--- src/OOP.java
+++ src/OOP.java
@@ -40,4 +40,17 @@ public class OOP {
 //    };
 //
 
+
+// TODO Java Method Overriding 2 (Super Keyword)
+//     When a method in a subclass overrides a method in superclass, it is still possible to call the overridden method using super keyword. If you write super.func() to call the function func(), it will call the method that was defined in the superclass.
+//
+//You are given a partially completed code in the editor. Modify the code so that the code prints the following text:
+//Hello I am a motorcycle, I am a cycle with an engine.
+//My ancestor is a cycle who is a vehicle with pedals.
+//
+//
+//String temp=super.define_me();
+//
+
+
 }
